fd095d1d962ba4b03c56d5a07e6bf685207b774b
Suyog Soti <ssoti2201@bvsd.org>
2014-12-09 17:04:00 -0700

    some theme work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root 'static_pages#home'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
