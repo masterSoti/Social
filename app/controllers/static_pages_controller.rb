7e067140a85e17d32c9d5acb2b1af565b5f1f9c6
Suyog Soti <ssoti2201@bvsd.org>
2014-12-15 19:58:16 -0700

    set link to static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     flash[:notice] = "Sorry, but this website is still under construction"
   end
+  def contact
+    flash[:notice] = "Sorry, but this website  is still under constuction"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get 'static_pages/contact', :as => 'contact_page'
   root 'static_pages#home'
   resources :users
 
